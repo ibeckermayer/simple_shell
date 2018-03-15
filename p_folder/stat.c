924b91b4d354c51ae96da154a52475daf84d41fe
Austin <aharshbe@live.com>
2018-03-15 16:27:34 -0700

    adding stat.c

---
+++ p_folder/stat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+char *find_path(char **environ)
+{
+	int i = 0, j = 0;
+
+	while (environ[i])
+	{
+		j = 0;
+		if (environ[i][j++] == 'P')
+		{
+			if (environ[i][j++] == 'A')
+			{
+				if (environ[i][j++] == 'T')
+				{
+					if (environ[i][j++] == 'H')
+					{
+						return (environ[i] + 5);
+					}
+				}
+			}
+		}
+		i++;
+	}
+}
+
+int main(int argc, char **argv, char **environ)
+{
+	char *_path = find_path(environ), *_paths = NULL;
+
+	_paths = strtok(_path, ":");
+
+	while (_paths != NULL)
+	{
+		printf("%s\n", _paths);
+		_paths = strtok(NULL, ":");
+	}
+
+}
