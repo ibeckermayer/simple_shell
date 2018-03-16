c735826b174f51ea49775e1c481a359da4fb6aff
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-03-16 22:40:19 +0000

    got a V2 working that can handle multiple arguments

---
+++ V2/_strcat.c
@@ -0,0 +1,33 @@
+#include "ss_head.h"
+
+/**
+* _strcat - concatinates two strings
+* @dest: string to be appende
+* @src: source string to append to
+* Return: th dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int size = 0, size2 = 0, j, k, total = 0;
+	char *final;
+
+	while (dest[size])
+		size++;
+
+	while (src[size2])
+		size2++;
+
+	final = malloc(total = size + size2 + 2);
+
+	while (dest[j])
+	{
+		final[j] = dest[j];
+		j++;
+	}
+	final[j++] = '/';
+
+	for (k = 0; j < total; j++, k++)
+		final[j] = src[k];
+
+	return (final);
+}
---
+++ V2/show_prompt.c
@@ -0,0 +1,12 @@
+#include "ss_head.h"
+
+/**
+* show_prompt - shows prompt
+* @buffer: buffer to store
+* Return: void
+*/
+
+void show_prompt(void)
+{
+	write(1, ";)$ ", 4);
+}
